7a5be5500fa3c10bfb0954ce1206c251ad354244
Thomas Tanner <trtanner@btinternet.com>
2020-03-22 09:40:33 +0000

    Reformat and some cleanup

--- source/chrome/content/openlinkSettings.js
+++ source/chrome/content/openlinkSettings.js
@@ -1,40 +1,52 @@
-var gOpenlinkPrefs = Components.classes["@mozilla.org/preferences-service;1"].
-				getService(Components.interfaces.nsIPrefService).getBranch("openlink.");
+/*jshint browser: true, devel: true */
+/*eslint-env browser */
 
+var gOpenlinkPrefs = Components.classes["@mozilla.org/preferences-service;1"].
+getService(Components.interfaces.nsIPrefService)
+  .getBranch("openlink.");
 
 /**
- * Called when settings dialog is displayed. Checks or unchecks the "use submenu" box according to
- * the user pref. If no such pref exists, box is unchecked. For robustness, pref is then (re)set according
- * to the state of the box.
+ * Called when settings dialog is displayed. Checks or unchecks the
+ * "use submenu" box according to the user pref. If no such pref exists, box is
+ * unchecked. For robustness, pref is then (re)set according to the state of the
+ * box.
  */
-function openlinkOnLoadSettingsDialog() {
-	var useLinkSubmenuBox = document.getElementById('openlink-uselinksubmenu');
-	if (useLinkSubmenuBox) {
-		//Deal with the box, defaulting to unchecking if there are any problems:
-		var checkLinkSubmenuBox = false;
-		if (gOpenlinkPrefs.getPrefType("useSubmenuForLinks") == gOpenlinkPrefs.PREF_BOOL) {
-			try {
-				if (gOpenlinkPrefs.getBoolPref("useSubmenuForLinks")) {
-					checkLinkSubmenuBox = true;
-				}
-			} catch (Exception) {
-			}
-		}
-		useLinkSubmenuBox.checked = checkLinkSubmenuBox;
-		//(Re)set pref according to the state of the box:
-		gOpenlinkPrefs.setBoolPref("useSubmenuForLinks", useLinkSubmenuBox.checked);
-	}
+function openlinkOnLoadSettingsDialog()
+{
+  var useLinkSubmenuBox = document.getElementById('openlink-uselinksubmenu');
+  if (useLinkSubmenuBox)
+  {
+    //Deal with the box, defaulting to unchecking if there are any problems:
+    var checkLinkSubmenuBox = false;
+    if (gOpenlinkPrefs.getPrefType("useSubmenuForLinks") == gOpenlinkPrefs
+      .PREF_BOOL)
+    {
+      try
+      {
+        if (gOpenlinkPrefs.getBoolPref("useSubmenuForLinks"))
+        {
+          checkLinkSubmenuBox = true;
+        }
+      }
+      catch (Exception)
+      {}
+    }
+    useLinkSubmenuBox.checked = checkLinkSubmenuBox;
+    //(Re)set pref according to the state of the box:
+    gOpenlinkPrefs.setBoolPref("useSubmenuForLinks", useLinkSubmenuBox.checked);
+  }
 }
-
 
 /**
  * Called when settings dialog is OK'd. Updates the user prefs.
  */
-function openlinkOnSettingsAccept() {
-	var useLinkSubmenu = false;
-	var useLinkSubmenuBox = document.getElementById('openlink-uselinksubmenu');
-	if (useLinkSubmenuBox) {
-		useLinkSubmenu = useLinkSubmenuBox.checked;
-	}
-	gOpenlinkPrefs.setBoolPref("useSubmenuForLinks", useLinkSubmenu);
+function openlinkOnSettingsAccept()
+{
+  var useLinkSubmenu = false;
+  var useLinkSubmenuBox = document.getElementById('openlink-uselinksubmenu');
+  if (useLinkSubmenuBox)
+  {
+    useLinkSubmenu = useLinkSubmenuBox.checked;
+  }
+  gOpenlinkPrefs.setBoolPref("useSubmenuForLinks", useLinkSubmenu);
 }
